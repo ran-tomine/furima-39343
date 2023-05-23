b5c31803b4fc52026c4b77a8a593a5e9c4225a75
ran02160 <ran02160ay@gmail.com>
2023-05-23 18:16:29 +0900

    エラーメッセージ表示修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
+    @user.first_name = '太郎'
+    @user.last_name = '山田'
   end
 
   describe 'ユーザー新規登録' do
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      expect(@user).to be_valid
+    end
+
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
