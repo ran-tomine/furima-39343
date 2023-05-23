ca1ad1fff726407433228880c2a5fb1157b97b19
ran02160 <ran02160ay@gmail.com>
2023-05-23 11:38:24 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_katakana, :last_katakana, :date_of_birth ])
   end
 
 
