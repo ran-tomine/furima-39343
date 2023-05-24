1f04ce9aa2cf48fbe74823bba9050cfd49f78550
ran02160 <ran02160ay@gmail.com>
2023-05-24 15:28:12 +0900

    修正４

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -112,23 +112,26 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Last katakana 全角文字を使用してください")
     end
 
-    it 'パスワードが半角英数字混合でなければ登録できない' do
+    it '英字のみのパスワードでは登録できない' do
       @user.password = 'password' # 英字のみのパスワード
       @user.password_confirmation = 'password'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password は半角英数字混合で設定してください')
+    end
 
+    it '数字のみのパスワードでは登録できない' do
       @user.password = '123456' # 数字のみのパスワード
       @user.password_confirmation = '123456'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password は半角英数字混合で設定してください')
+    end
 
+      it '全角文字を含むパスワードでは登録できない' do
       @user.password = 'パスワード１２３' # 全角文字を含むパスワード
       @user.password_confirmation = 'パスワード１２３'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password は半角英数字混合で設定してください')
     end
-
   end
   end
 end
