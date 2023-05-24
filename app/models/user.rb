22b1a229b777114d196d76add8e5918c28e99125
ran02160 <ran02160ay@gmail.com>
2023-05-24 11:01:52 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
   validates :first_katakana, :last_katakana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
   validates :date_of_birth, presence: true
-  validates :email, uniqueness: { case_sensitive: true }
 end
