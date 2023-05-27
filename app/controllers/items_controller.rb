cb0d5aeac0506912f7bdb82183556b33a3ce0fa3
ran02160 <ran02160ay@gmail.com>
2023-05-27 21:46:38 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,25 @@ class ItemsController < ApplicationController
    
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  
+  end
+
+  def update
+    @item = Item.find(params[:id])
+  
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def item_params
